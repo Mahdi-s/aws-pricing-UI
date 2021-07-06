e593454eff8ac228e04ddb59697ae48f23c7a396
Mahdi <44215423+Mahdi-s@users.noreply.github.com>
2021-07-06 15:58:54 -0500

    nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,11 @@ import React from "react";
 function NavBar() {
 
     return (
-        <nav class='navbar navbar-expand-lg navbar-light bg-light justify-content-between'>        
+        <nav class='navbar navbar-expand-lg navbar-light bg-light justify-content-between'>   
+        <span>     
         <a class="navbar-brand" href="#"> <i class="fas fa-cloud"></i> MCP Customer Cloud AWS Portal </a>
-        
+        </span>
+        <div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -39,6 +41,8 @@ function NavBar() {
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
+        </div>
+        
     </nav>
     )
 
