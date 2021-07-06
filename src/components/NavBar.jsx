30b87595706f2566953171925be0a167b13fd243
Mahdi <44215423+Mahdi-s@users.noreply.github.com>
2021-07-06 15:11:40 -0500

    more changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,14 @@ import React from "react";
 function NavBar() {
 
     return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class='navbar navbar-expand-lg navbar-light bg-light justify-content-between'>        
         <a class="navbar-brand" href="#"> <i class="fas fa-cloud"></i> MCP Customer Cloud AWS Portal </a>
         
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse d-flex" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
